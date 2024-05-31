c0cea611ea4e2ad3e5f6c18486ffd233524af07d
angel <angel@DESKTOP-8ELEIVF>
2024-05-30 22:48:10 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Presiname de nuevo";   
+        }
+    }
+
+}
